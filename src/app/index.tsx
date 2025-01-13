fb7e653699dd3575bf68c0093dce3f29f372a54b
Gilead Odo <odogilead@gmail.com>
2025-01-13 12:39:20 +0100

    Initialize project structure and add configuration files for iOS and Android

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link, router } from 'expo-router';
+import { View, Text } from '@/components/general/Themed';
+import CustomButton from '@/components/general/CustomButton';
+import WorkoutListItem from '@/components/workouts/WorkoutListItem';
+import { FlatList } from 'react-native';
+import { useWorkouts } from '@/store';
 
-export default function Page() {
+export default function HomeScreen() {
+  const currentWorkout = useWorkouts((state) => state.currentWorkout);
+  const startWorkout = useWorkouts((state) => state.startWorkout);
+  const workouts = useWorkouts((state) => state.workouts);
+
+  const onStartWorkout = () => {
+    startWorkout();
+    router.push('/workout/current');
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+    <View
+      style={{
+        flex: 1,
+        gap: 10,
+        padding: 10,
+        backgroundColor: 'transparent',
+      }}
+    >
+      {currentWorkout ? (
+        <Link href="/workout/current" asChild>
+          <CustomButton title="Resume workout" />
+        </Link>
+      ) : (
+        <CustomButton title="Start new workout" onPress={onStartWorkout} />
+      )}
+
+      <FlatList
+        data={workouts}
+        contentContainerStyle={{ gap: 8 }}
+        renderItem={({ item }) => <WorkoutListItem workout={item} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
